be9c559cee834dd9a5c9e27963e18793230fd0c7
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-02-06 10:10:00 +0000

    Create fib.py
    Week 1 - Exercise (Fibonacci exercise)

---
+++ fib.py
@@ -0,0 +1,18 @@
+# Noa P Prada Schnor W1 exercise
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+# Test the function with the following value.
+x = 15
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
