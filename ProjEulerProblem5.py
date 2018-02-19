1a7586b38a38ed089cc60a0cfa558b23909557f3
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-02-19 11:57:30 +0000

    Update ProjEulerProblem5.py

--- ProjEulerProblem5.py
+++ ProjEulerProblem5.py
@@ -5,7 +5,7 @@ i = range(1,21) #"list" from 1 to 20
 f = 0 #number that I need to find out
 
 while i <= 20: #while the number of my created list is <=20
-  if f % i == 0: #even number
-  f = f + 1 #increase the number that I need to find out
+  if f % i == 0: #f must be an even number
+  f = f + 1 #every time the loop happens it must increase the number that I need to find out
 
   print(f) #smallest positive number that is evenly divisible by all of the numbers from 1 to 20
