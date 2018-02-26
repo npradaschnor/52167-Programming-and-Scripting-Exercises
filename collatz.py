7cbe176a1511c6466c8eb4dd4ba56e0e73252615
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-02-26 14:03:31 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,11 +1,11 @@
 #Noa Prada Schnor 28-02-06
-#Collatz Conjecture
+#Collatz Conjecture (https://en.wikipedia.org/wiki/Collatz_conjecture)
 #Week 3 Exercise
 
-nominal = int(input("Enter integer:"))
-while nominal!=1:
+nominal = int(input("Enter integer:")) #enter a number
+while nominal!=1: #while number is different from 1
     if nominal % 2 ==0: #if the previous term is even, the next term is one half the previous term
-        nominal = nominal/2
+        nominal = nominal//2
         print (nominal) #Otherwise, the next term is 3 times the previous term plus 1
     else:
         nominal = (nominal * 3) + 1
