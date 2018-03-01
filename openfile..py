f715220f353204a163ec8ea8412f33529f2d31f8
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-01 12:02:02 +0000

    Update openfile..py

--- openfile..py
+++ openfile..py
@@ -1,8 +1,9 @@
-#Noa P Prada Schnor  2018-02-28
+#Noa P Prada Schnor  2018-03-01
+#Print decimal places aligned, with a space between the columns
 
 import csv
 
 with open('data/iris.csv', newline='') as csvFile:
   for line in csvFile:
-    line = line.replace(',', ' ')
-    print(line[:3]+ '      ' + line[4:7]+ '      ' + line[8:11]+ '      ' + line[12:15])
+    line = line.replace(',', ' ') #no comma separating the numbers
+    print(line[:3]+ '      ' + line[4:7]+ '      ' + line[8:11]+ '      ' + line[12:15]) #print an output splitting the line and adding space between them
