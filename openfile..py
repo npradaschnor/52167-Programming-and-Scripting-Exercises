e70b131798426ab8b1344df4a280de34201a136e
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-01 11:57:23 +0000

    Update openfile..py

--- openfile..py
+++ openfile..py
@@ -1,8 +1,8 @@
 #Noa P Prada Schnor  2018-02-28
-#Adapted from Mohamed Noor
- 
-with open("data/iris.csv", 'r') as f:
-  for line in f:
-    line = line.replace(',', ' ') #returns a copy of the string in which the occurrences of old (',') have been replaced with new('')
-    line = line.rstrip() #strings are immutable, so creates a new string instance with the stripped content
-    print('{0[0]:1} {0[1]:1} {0[2]:1} {0[3]:1} {0[4]:1} {0[5]:1} {0[6]:1} {0[7]:1} {0[8]:1} {0[9]:1} {0[10]:1} {0[11]:1} {0[12]:1} {0[13]:1} {0[14]:1} {0[15]:1}'.format(line))
+
+import csv
+
+with open('data/iris.csv', newline='') as csvFile:
+  for line in csvFile:
+    line = line.replace(',', ' ')
+    print(line[:3]+ '    ' + line[4:7]+ '    ' + line[8:11]+ '    ' + line[12:15])
