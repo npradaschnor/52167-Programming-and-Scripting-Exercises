6e80ff1864264399eed25dbdcf3be51cc5291119
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-03 12:39:20 +0000

    Update ProjEulerProblem5.py

--- ProjEulerProblem5.py
+++ ProjEulerProblem5.py
@@ -1,14 +1,14 @@
 #Noa P Prada Schnor, 2018-02-19
 #Project Euler Problem 5 (https://projecteuler.net/problem=5)
 
-i = 2 #"list" from 1 to 20
-f = 2520 #number that I need to find out
+i = 2 #number starts from 2
+f = 2 #number that I need to find out
 
 while i < 21: #while the number of my created list is <=20
   if f % i == 0: #f must be an even number
    i = i + 1
   else:
-    n=n+1
+    f=f+1
     i=2
 
-  print('The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is:',f) 
+print('The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is:',f) 
