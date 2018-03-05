3d90798192ba3d0c008e6f94da7a1b13099fed9f
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-05 16:14:09 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,25 +1,29 @@
 #Noa P Prada Schnor 2018-03-05
 
+#option 1
 def factorial(i):
-    number = 1
-    while i >= 1:
-        number = number * i
-        i = i - 1
-    return number
+    number = 1 #number is the result
+    while i >= 1: #i is the positive integer that it will return its factorial.While the positive integer is =1
+        number = number * i #result is the number multiplied by the positive integer
+        i = i - 1 #the positive integer should be minus 1
+    return number #return number and continue the loop while i is equal 1, then stop the loop
 print(factorial(5))
 print(factorial(7))
 print(factorial(10))
 
-from math import factorial
+#option 2
+def factorial(integer): #positive integer that will return its factorial
+    if integer < 2: #if integer is less than 2
+        return 1 #consider integer as 1
+    return integer * factorial(integer - 1) #return the positive integer multiplying by itself -1
+print(factorial(5))
+print(factorial(7))
+print(factorial(10))
+
+#shortcut
+from math import factorial #shortcut with no need to create a defining function
 print(factorial(5))
 print(factorial(7))
 print(factorial(10))
 
 
-def factorial(integer):
-    if integer < 2:
-        return 1
-    return integer * factorial(integer - 1)
-print(factorial(5))
-print(factorial(7))
-print(factorial(10))
