59ab13a6bbffa173181ab666f4cdf8e6bab96a41
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-05 16:03:12 +0000

    Create factorial.py
    Write a Python script containing a function called factorial() that takes a single input/argument which is a positive integer and returns its factorial. The factorial of a number is that number multiplied by all of the positive numbers less than it. For example, the factorial of 5 is 5x4x3x2x1 which equals 120. You should, in your script, test the function by calling it with the values 5, 7, and 10.

---
+++ factorial.py
@@ -0,0 +1,19 @@
+#Noa P Prada Schnor 2018-03-05
+
+def factorial(i):
+    number = 1
+    while i >= 1:
+        number = number * i
+        i = i - 1
+    return number
+print(factorial(5))
+
+from math import factorial
+print(factorial(5))
+
+
+def factorial(integer):
+    if integer < 2:
+        return 1
+    return integer * factorial(integer - 1)
+print(factorial(5))
