2cd9d96e0b4f69588d51a45d08a733fc72d68ecd
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-07 16:45:09 +0000

    Update openfileV2.py

--- openfileV2.py
+++ openfileV2.py
@@ -7,3 +7,19 @@ with open("data/iris.csv") as f:
     # coord = (3, 5)
     # 'X: {0[0]};  Y: {0[1]}'.format(coord)
     # 'X: 3;  Y: 5'
+
+  
+  #Noa P Prada Schnor 2018-03-07
+  
+  Def makefloat(teststr): #Gerhard Van Der Linde def Function string to number
+  try:
+    return float(teststr)  # return a floating point value if possibe.
+  except:
+    return teststr  # otherwise return the string as is
+
+
+with open("data/iris.csv") as data:
+  makefloat(data)
+  for line in data:
+    line = line.split(',')
+    print('{0:10} {0:10} {0:10} {0:10}'.format(line[0], line[1], line[2], line[3]))
